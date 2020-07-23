75a1497d42a75c298843e71fe06ba2540520d586
nekosakana <kiri33pudding@gmail.com>
2020-07-24 01:40:14 +0900

    ログイン・ログアウト、マイページへ遷移

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8ef3405e5604072618798f90c3447907d8876fd585a9ab76ca97c8e199de1f189250a5af00ef2d578d8dde25ac39164265cd66e2ba42bcb5530e7e7044a92b77'
+
