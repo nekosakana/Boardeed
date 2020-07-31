8cd936430677df6052b6c52dd7d76623e81bfef1
nekosakana <kiri33pudding@gmail.com>
2020-07-31 17:55:40 +0900

    記事検索、条件検索、タグ検索機能追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,4 +9,26 @@ class Blog < ApplicationRecord
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
+
+    def Blog.search(search)
+        if search
+           Blog.where(['title LIKE ?', "%#{search}%"])
+           Blog.where(['body LIKE ?', "%#{search}%"])
+        else
+           Blog.all
+        end
+    end
+
+    def Blog.check_search(play_people, play_time)
+        if play_people && play_time
+            Blog.joins(:game).where(games: {play_people: play_people, play_time: play_time})
+        elsif play_people
+            Blog.joins(:game).where(games: {play_people: play_people})
+        elsif play_time
+            Blog.joins(:game).where(games: {play_time: play_time})
+        else
+            Blog.all
+        end
+        
+    end
 end
