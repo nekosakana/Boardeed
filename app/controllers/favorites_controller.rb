cdacc1341d9314ed311632f25a8f1fb45765cd28
nekosakana <kiri33pudding@gmail.com>
2020-08-12 12:57:35 +0900

    いいねの非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
         blog = Blog.find(params[:blog_id])
         favorite = current_user.favorites.new(blog_id: blog.id)
         favorite.save
-        redirect_back(fallback_location: root_path)
+        @blog = Blog.find(params[:blog_id])
     end
 
     def destroy
         blog = Blog.find(params[:blog_id])
         favorite = current_user.favorites.find_by(blog_id: blog.id)
         favorite.destroy
-        redirect_back(fallback_location: root_path)
+        @blog = Blog.find(params[:blog_id])
     end
 end
